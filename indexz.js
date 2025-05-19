dc4dc6d6b8771c3bbe3eea161880a3e99ab9ab13
Zinabu-Kelemework <zamdlove@gmail.com>
2025-05-20 01:03:37 +0300

    Update indexz.js

--- indexz.js
+++ indexz.js
@@ -7,7 +7,7 @@ const app = express();
 
 // MongoDB connection
 
-mongoose.connect('mongodb+srv://zamdlove:Simacloud7@cluster0.vb21kbo.mongodb.net/zinabu' {
+mongoose.connect('mongodb+srv://Zinabu:Simacloud7@cluster0.vb21kbo.mongodb.net/zinabu?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
