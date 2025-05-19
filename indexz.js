ba0b395ab501bb316acca47b9ed2d6b7bf78b556
Zinabu-Kelemework <zamdlove@gmail.com>
2025-05-20 00:06:07 +0300

    Update indexz.js

--- indexz.js
+++ indexz.js
@@ -6,7 +6,6 @@ const path = require('path');
 const app = express();
 
 // MongoDB connection
-const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://<username>:<password>@cluster0.mongodb.net/photo_gallery_app', {
   useNewUrlParser: true,
